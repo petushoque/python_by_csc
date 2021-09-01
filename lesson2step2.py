4d69b18fa85c578e36a09c91f0c762ee234c35af
Kubasov Alexei <kubasovalexei@yandex.ru>
2021-09-01 10:48:52 +0700

    Update lesson2step2.py

--- lesson2step2.py
+++ lesson2step2.py
@@ -2,19 +2,9 @@
 import urllib.request
 
 #открыть страницу
-fid=urllib.request.urlopen('https://stepik.org/media/attachments/lesson/209717/1.html')
+fid=urllib.request.urlopen('https://stepik.org/media/attachments/lesson/209719/2.html')
 
 #записать в переменную
-webpage=fid.read().decode('utf-8')
+webpage=fid.read().decode('utf-8').lower()
 
-#посчитать количество упоминаний
-cpp = str(webpage).count('C++')
-python = str(webpage).lower().count('python')
-
-#вывести более часто встречающийся язык
-if cpp > python:
-    print('C++')
-elif cpp < python:
-    print('python')
-else:
-    print('')
+print(webpage)
