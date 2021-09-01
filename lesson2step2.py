6baddad39f043c6cf2607e6e9f95a1b83e1d00a5
Kubasov Alexei <kubasovalexei@yandex.ru>
2021-09-01 10:54:25 +0700

    Update lesson2step2.py

--- lesson2step2.py
+++ lesson2step2.py
@@ -7,4 +7,13 @@ fid=urllib.request.urlopen('https://stepik.org/media/attachments/lesson/209719/2
 #записать в переменную
 webpage=fid.read().decode('utf-8').lower()
 
+state = 0
+temp = ''
+
+for c in webpage:
+    if c == '<' and temp == '':
+        temp += '<'
+    if c = 'c' and temp == '<':
+        temp += 'c'
+    
 print(webpage)
