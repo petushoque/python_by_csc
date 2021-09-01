7709dae55f48386d2ba7c3bdc81022c7bb9be35c
Kubasov Alexei <kubasovalexei@yandex.ru>
2021-09-01 11:21:33 +0700

    Update lesson2step2.py

--- lesson2step2.py
+++ lesson2step2.py
@@ -7,29 +7,12 @@ fid=urllib.request.urlopen('https://stepik.org/media/attachments/lesson/209719/2
 #записать в переменную
 webpage=fid.read().decode('utf-8').lower()
 
-state = 0
-temp = ''
+code_list = []
 
-for c in webpage:
-    if c == '<' and temp == '':
-        temp += '<'
-    elif c == 'c' and temp == '<':
-        temp += 'c'
-    elif c == 'o' and temp == '<c':
-        temp += 'o'
-    elif c == 'd' and temp == '<co':
-        temp += 'd'
-    elif c == 'e' and temp == '<cod':
-        temp += 'e'
-    elif c == '>' and temp == '<code':
-        temp += '>'
-    if temp == '<code>':
-        print(temp)
-        state += 1
-        temp = ''
-        print(state)
-        
-code_string = webpage.count('<code>')
+while webpage.find('<code>') > 0:
+    code_start = webpage.find('<code>')
+    code_end = webpage.find('</code>')
 print(code_string)
+print(webpage[17712:17720])
 
 print(state)
