e512dcbc796659099855b42dd94c6ed2830b6ca2
Kubasov Alexei <kubasovalexei@yandex.ru>
2021-09-01 17:23:24 +0700

    Update lesson2step2.py

--- lesson2step2.py
+++ lesson2step2.py
@@ -9,19 +9,12 @@ webpage=fid.read().decode('utf-8').lower()
 
 code_list = []
 
-print(webpage.count('<code>'))
-
-# почему появляется пустой 6 элемент?
-
 while webpage.find('<code>') > -1:
     code_start = webpage.find('<code>')
     code_end = webpage.find('</code>')
-    code = webpage[code_start:code_end + 7]
+    code = webpage[code_start + 6:code_end]
     # пропускать пустые элементы
     if len(code) > 0:
-        code_list.append(code)
-    print('элементов в листе:', len(code_list), code)
-    
+        code_list.append(code)    
     webpage = webpage[code_end + 7:]
 
-print(len(code_list))
